4244e0321317f10d56127c2418d2c2065e53c9dc
Steve Meisburger <spm82@cornell.edu>
2024-02-01 11:40:55 -0500

    updated fast_dp.json keys for new version

--- Main.py
+++ Main.py
@@ -14,28 +14,13 @@ LOG_FILES = [
     'autoindex.log',
     ]
 
-COLUMN_NAMES = {
-    "unit_cell":"Unit Cell",
-    "spacegroup":"Space Group",
-    "data":"Dataset",
-    'xml_results.completeness_overall':'Completeness',
-    'xml_results.multiplicity_overall':'Multiplicity',
-    'xml_results.isigma_overall':'I/σI',
-    'xml_results.resol_high_overall':'Resolution',
-    'xml_results.rmeas_overall':'Rmeas',
-}
+# column names
+CELL, SG, DS = 'Unit Cell', 'Space Group', 'Dataset'
+COMP, MULT = 'Completeness', 'Multiplicity'
+ISIGI, RESOL, RMERGE = 'I/σI', 'Resolution', 'Rmerge'
 
-# columns to include, and the order to include them in
-COLUMNS = [
-    'data',
-    'spacegroup',
-    'unit_cell',
-    'xml_results.resol_high_overall',
-    'xml_results.isigma_overall',
-    'xml_results.completeness_overall',
-    'xml_results.multiplicity_overall',
-    'xml_results.rmeas_overall',
-]
+# columns in order
+COLUMNS = [DS, SG, CELL, RESOL, ISIGI, COMP, MULT, RMERGE]
 
 st.set_page_config(layout="wide")
 
@@ -58,12 +43,45 @@ with st.sidebar:
     st.button('refresh')
 
 def find_fast_dp_folders():
-    fast_dp_folders = glob.glob('**/*_fast_dp/',recursive=True)
+    fast_dp_folders = glob.glob('**/*fast_dp/',recursive=True)
     a = {}
     for f in fast_dp_folders:
         b = {v:os.path.join(f,v) for v in LOG_FILES if os.path.exists(os.path.join(f,v))}
-        a[f.replace('_fast_dp/','')] = b
+        fname = f.replace('_fast_dp/','').replace('fast_dp/','') # covers legacy folder names
+        a[fname] = b
     return a
+
+def parse_fast_dp_results(fast_dp):
+    """convert dictionary from fast_dp.json to dict with standard keys"""
+
+    results = {k:None for k in COLUMNS}
+    results[CELL] = fast_dp.get('unit_cell',None),
+    results[SG] = fast_dp.get('spacegroup',None),
+    results[DS] = fast_dp.get('data',None)
+    
+    if 'scaling_statistics' in fast_dp: # new version
+        stats = fast_dp['scaling_statistics'].get('overall',{})
+        
+        results[COMP] = stats.get('completeness',None)
+        results[MULT] = stats.get('multiplicity',None)
+        results[ISIGI] = stats.get('mean_i_sig_i',None)
+        results[RESOL] = stats.get('res_lim_high',None)
+        results[RMERGE] = stats.get('r_merge',None)
+        
+    elif 'xml_results' in fast_dp: # old version
+        stats = fast_dp['xml_results']
+        
+        results[COMP] = stats.get('completeness_overall',None)
+        results[MULT] = stats.get('multiplicity_overall',None)
+        results[ISIGI] = stats.get('isigma_overall',None)
+        results[RESOL] = stats.get('resol_high_overall',None)
+        results[RMERGE] =stats.get('rmerge_overall',None)
+        
+    else: # not recognized
+        pass
+        
+    return results
+
 
 st.header('Summary statistics')
 st.write('Select a row to view log files')
@@ -71,6 +89,7 @@ st.write('Select a row to view log files')
 s = find_fast_dp_folders()
 
 a = []
+
 for k,v in s.items():
     if 'fast_dp.json' in v:
         with open(v['fast_dp.json']) as f:
@@ -78,17 +97,19 @@ for k,v in s.items():
     else:
         fast_dp = {}
     fast_dp['data'] = k
+    fast_dp = parse_fast_dp_results(fast_dp)
     a.append(fast_dp)
+    
+if len(a)==0:
+    st.warning('No fast_dp folders found in directory.')
+    st.stop()
 
 df = pd.json_normalize(a)
+print(df)
 
 df = df[COLUMNS]
 
 gb = GridOptionsBuilder.from_dataframe(df)
-for k,v in COLUMN_NAMES.items():
-    if k in COLUMNS:
-        gb.configure_column(k, header_name=v)
-
 gb.configure_selection('single',use_checkbox=False)
 go = gb.build()
 
@@ -102,7 +123,7 @@ grid_response = AgGrid(
 selected = grid_response['selected_rows']
 
 if selected:
-    dataset = selected[0]['data']
+    dataset = selected[0][DS]
     st.header(dataset)
     for k,v in s[dataset].items():
         with st.expander(k,expanded=False):
